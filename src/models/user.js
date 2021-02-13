d7cc64df8034a75aaac428b78781fb665dde4946
Thomas <digitalwizard@comcast.net>
2021-02-13 16:23:34 -0600

    Initial commit

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        trim: true,
+        validate(value) {
+            if (value.includes('password')) {
+                throw new Error("Do not use 'password' as part of the password")
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age cannot be negative')
+            }
+        }
+    }
+})
+
+module.exports = User
